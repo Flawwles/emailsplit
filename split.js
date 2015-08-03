1caaba0a24cc6b2cba3490658338c442f386cfd7
Matt Evans <matt_-@hotmail.co.uk>
2015-08-03 21:56:54 +0100

    Add package.json and split.js

---
+++ split.js
@@ -0,0 +1,17 @@
+var cheerio = require('cheerio'),
+    fs = require('fs');
+
+fs.readFile('complex.html', 'utf8', dataLoaded);
+
+function dataLoaded(err, data) {
+    $ = cheerio.load(' ' + data + ' ');
+    $('.backgroundTable').each(function(i, elem) {
+        var id = $(elem).attr('class'),
+            filename = "./export/blocks/" + id + i + '.html',
+            content = $.html(elem);
+        fs.writeFile(filename, content, function(err) {
+            console.log('Written html to ' + filename);
+        });
+    });
+}
+
