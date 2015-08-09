92124bc1fc4995e72cd8b3df476322be0988c96c
JakeSidSmith <jake@dabapps.com>
2015-08-09 23:31:04 +0100

    Fix renderer typo

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 var splitter = require('./src/splitter');
 var clean = require('./src/clean');
-var rednerer = require('./src/renderer');
+var renderer = require('./src/renderer');
 
 clean.do('./export/blocks/*.html');
 clean.do('./export/images/*.png');
 splitter.do('live.html', function() {});
-rednerer.do();
-
-
+renderer.do();
