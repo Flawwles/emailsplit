6b6fc9813269e2ebec37d349b3552d5b6106833b
JakeSidSmith <jake@dabapps.com>
2015-08-10 00:05:53 +0100

    Log help if no file name supplied

--- index.js
+++ index.js
@@ -5,17 +5,20 @@
   var args = [].concat(process.argv);
   var program = args.splice(0, 1)[0];
   var directory = args.splice(0, 1)[0];
-  var fileName = args.splice(0, 1)[0] || 'live.html';
-  var className = '.'.concat(args.splice(0, 1)[0] || 'backgroundTable');
+  var fileName = args.splice(0, 1)[0] || '';
+  var className = '.'.concat(args.splice(0, 1)[0] || '');
 
-  var splitter = require('./src/splitter');
-  var clean = require('./src/clean');
-  var renderer = require('./src/renderer');
+  if (!fileName) {
+    console.log('\n  Usage: emailsplit <fileName> <className>\n');
+  } else {
+    var splitter = require('./src/splitter');
+    var clean = require('./src/clean');
+    var renderer = require('./src/renderer');
 
-  clean.do('./export/blocks/*.html');
-  clean.do('./export/images/*.png');
-  splitter.do(fileName, className, function() {});
-  renderer.do();
-
+    clean.do('./export/blocks/*.html');
+    clean.do('./export/images/*.png');
+    splitter.do(fileName, className, function() {});
+    renderer.do();
+  }
 
 })();
